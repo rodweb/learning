8b525aa2c42cd72fed37a0284cfd49d4846a54a4
Rodrigo Campos <rod.apd@gmail.com>
2024-04-14 10:42:36 -0300

    feat: support multiple chats

--- supabase/functions/telegram-handler/index.ts
+++ supabase/functions/telegram-handler/index.ts
@@ -1,12 +1,12 @@
 console.log(`Function "telegram-bot" up and running!`)
 
 import { Bot, webhookCallback } from 'https://deno.land/x/grammy@v1.22.4/mod.ts'
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { supermemo, SuperMemoItem, SuperMemoGrade } from "https://deno.land/x/supermemo@2.0.17/mod.ts";
-import { Entry } from './model.ts';
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.3";
+import { supermemo } from "https://deno.land/x/supermemo@2.0.17/mod.ts";
+import { Database } from './schema.ts'
 
 const bot = new Bot(Deno.env.get('TELEGRAM_BOT_TOKEN') || '')
-const supabase = createClient(Deno.env.get('SUPABASE_URL') || '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || '')
+const supabase = createClient<Database>(Deno.env.get('SUPABASE_URL') || '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || '')
 
 bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
 
@@ -16,7 +16,7 @@ bot.reaction('❤', async (ctx) => {
   const { data, error } = await supabase
     .from('entries')
     .select()
-    .match({ message_id: ctx.messageReaction?.message_id })
+    .match({ chat_id: ctx.chat.id, message_id: ctx.messageReaction?.message_id })
 
   if (error) {
     console.log('Failed to fetch entry', error)
@@ -27,7 +27,7 @@ bot.reaction('❤', async (ctx) => {
     return ctx.reply('No entry found')
   }
 
-  const entry = data[0] as Entry
+  const entry = data[0]
   const { interval, repetition, efactor } = supermemo(entry, 5)
 
   const { error: updateError } = await supabase
@@ -53,6 +53,7 @@ bot.command('review', async (ctx) => {
   const { data, error } = await supabase
     .from('entries')
     .select()
+    .match({ chat_id: ctx.chat.id })
     .lte('due_date', new Date().toISOString())
     .limit(limit)
 
@@ -78,6 +79,7 @@ bot.command('review', async (ctx) => {
     .filter(({ error }) => Boolean(error))
     .map(({ reply, error }) => {
       console.log('Failed to update entry', error)
+      // deno-lint-ignore no-explicit-any
       return ctx.react('🤡', { chat_id: reply.chat.id, message_id: reply.message_id } as any)
     })
 
@@ -89,7 +91,7 @@ bot.on('message', async (ctx) => {
 
   const { error } = await supabase
     .from('entries')
-    .insert({ data: ctx.message.text })
+    .insert({ chat_id: ctx.chat.id, data: ctx.message.text || '' })
 
   if (error) {
     console.log(error)
@@ -131,8 +133,8 @@ async function initialize() {
   ])
 }
 
-function nextDueDate(interval: number): Date {
+function nextDueDate(interval: number): string {
   const d = new Date();
   d.setDate(d.getDate() + interval);
-  return d;
+  return d.toISOString();
 }
