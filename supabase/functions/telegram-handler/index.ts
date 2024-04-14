1322e0c9dee71a86c3e6f06fcd157d50199027da
Rodrigo Campos <rod.apd@gmail.com>
2024-04-14 09:37:44 -0300

    refactor: extract initialization code

--- supabase/functions/telegram-handler/index.ts
+++ supabase/functions/telegram-handler/index.ts
@@ -4,49 +4,26 @@ import { Bot, webhookCallback } from 'https://deno.land/x/grammy@v1.22.4/mod.ts'
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(Deno.env.get('SUPABASE_URL') || '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || '')
-const url = Deno.env.get('FUNCTION_URL') || ''
+
+let initialized = false
 
 const bot = new Bot(Deno.env.get('TELEGRAM_BOT_TOKEN') || '')
-bot.api.setWebhook(url, { allowed_updates: ['message', 'message_reaction'] })
-bot.api.setMyCommands([
-  { command: 'ping', description: 'Test the bot' },
-  { command: 'review', description: 'Review an entry' },
-])
 
 bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
 
 bot.reaction('❤', async (ctx) => {
   console.log('Reaction to message', ctx)
-  const { data, error } = await supabase
-    .from('entries')
-    .select()
-    .filter('message_id', 'eq', ctx.messageReaction?.message_id)
-    .limit(1)
-
-  if (error) {
-    console.log(error)
-    ctx.reply('Failed to fetch entry')
-    return
-  }
-
-  if (!data || data.length === 0) {
-    ctx.reply('No entries to review')
-    return
-  }
-
-  const entry = data[0]
-  const { error: updateError } = await supabase
+  const { error } = await supabase
     .from('entries')
     .update({ reviewed_at: new Date() })
-    .match({ id: entry.id })
+    .match({ message_id: ctx.messageReaction?.message_id })
 
-  if (updateError) {
-    console.log(updateError)
-    ctx.reply('Failed to update entry')
-    return
+  if (error) {
+    console.log('Failed to update entry', error)
+    return ctx.reply('Failed to update entry')
   }
 
-  ctx.react('👍')
+  return ctx.react('👍')
 })
 
 bot.command('review', async (ctx) => {
@@ -55,36 +32,41 @@ bot.command('review', async (ctx) => {
   if (typeof ctx.match === 'string') {
     limit = parseInt(ctx.match, 10)
   }
+
   const { data, error } = await supabase
     .from('entries')
     .select()
-    .or('reviewed_at.is.null,reviewed_at.lte.'+daysAgo(1).toISOString())
+    .or('reviewed_at.is.null,reviewed_at.lte.' + daysAgo(1).toISOString())
     .order('reviewed_at', { nullsFirst: true })
     .limit(limit)
 
   if (error) {
-    console.log(error)
-    ctx.reply('Failed to fetch entry')
-    return
+    console.log('Failed to fetch entry', error)
+    return ctx.reply('Failed to fetch entry')
   }
 
   if (!data || data.length === 0) {
-    ctx.reply('No entries to review')
-    return
+    return ctx.reply('No entries to review')
   }
 
-  for (const entry of data) {
+  const updates = await Promise.all(data.map(async (entry) => {
     const reply = await ctx.reply(entry.data)
-    const { error: updateError } = await supabase
+    const { error } = await supabase
       .from('entries')
       .update({ message_id: reply.message_id })
       .match({ id: entry.id })
-    if (updateError) {
-      console.log(updateError)
-      ctx.reply('Failed to update entry')
-      return
-    }
-  }
+      return { reply, error }
+  }))
+
+  const replies = updates
+    .filter(({ error }) => Boolean(error))
+    .map(({ reply, error }) => {
+      console.log('Failed to update entry', error)
+      bot.api.setMessageReaction(reply.chat.id, reply.message_id, ['👎' as any])
+      return ctx.react('👎', { chat_id: reply.chat.id, message_id: reply.message_id } as any)
+    })
+
+    return Promise.all(replies)
 })
 
 bot.on('message', async (ctx) => {
@@ -106,6 +88,11 @@ const handleUpdate = webhookCallback(bot, 'std/http')
 
 Deno.serve(async (req) => {
   try {
+    if (!initialized) {
+      initialize()
+      initialized = true
+    }
+
     const url = new URL(req.url)
     if (url.searchParams.get('secret') !== Deno.env.get('FUNCTION_SECRET')) {
       return new Response('not allowed', { status: 405 })
@@ -116,6 +103,17 @@ Deno.serve(async (req) => {
     console.error(err)
   }
 })
+
+async function initialize() {
+  const url = Deno.env.get('FUNCTION_URL') || ''
+  await Promise.all([
+    bot.api.setWebhook(url, { allowed_updates: ['message', 'message_reaction'] }),
+    bot.api.setMyCommands([
+      { command: 'ping', description: 'Test the bot' },
+      { command: 'review', description: 'Review an entry' },
+    ]),
+  ])
+}
 
 function daysAgo(n: number): Date {
   const d = new Date();
