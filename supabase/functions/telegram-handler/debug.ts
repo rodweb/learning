37a0351d9ba42e90025d9d13d37a2fb2d306e035
Rodrigo Campos <rod.apd@gmail.com>
2024-04-20 23:44:17 -0300

    fix: wait for debug promise again

--- supabase/functions/telegram-handler/debug.ts
+++ supabase/functions/telegram-handler/debug.ts
@@ -3,10 +3,10 @@ import { Bot, Context } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
 
 export function debug(token: string): Middleware {
     const bot = new Bot(token);
-    return function debugMiddleware(ctx: Context, next: NextFunction) {
+    return async function debugMiddleware(ctx: Context, next: NextFunction) {
         console.log('Update', ctx)
         // Do not wait for the promise to resolve
-        ctx.getChat()
+        await ctx.getChat()
             .then(chat => bot.api.sendMessage(chat.id, Deno.inspect(ctx)))
             .catch(console.error)
         return next()
