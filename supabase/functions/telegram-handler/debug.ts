2716d6ed36228e2e67adf8c2d07fa96e551a5cf2
Rodrigo Campos <rod.apd@gmail.com>
2024-04-21 14:46:46 -0300

    feat: send debug as html code

--- supabase/functions/telegram-handler/debug.ts
+++ supabase/functions/telegram-handler/debug.ts
@@ -5,9 +5,10 @@ export function debug(token: string): Middleware {
     const bot = new Bot(token);
     return function debugMiddleware(ctx: Context, next: NextFunction) {
         console.log('Update', ctx)
+        const code = `<code>${JSON.stringify(ctx, null, 2)}</code>`
         // Do not wait for the promise to resolve
         ctx.getChat()
-            .then(chat => bot.api.sendMessage(chat.id, JSON.stringify(ctx, null, 2)))
+            .then(chat => bot.api.sendMessage(chat.id, code, { parse_mode: 'HTML' }))
             .catch(console.error)
         return next()
     }
